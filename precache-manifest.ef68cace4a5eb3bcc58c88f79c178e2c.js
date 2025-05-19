a95395ea565d15005d5c6fa80c31f858ffff258f
Sanjeev <sanjeevnair@gmail.com>
2025-05-19 22:10:58 +0530

    Updates

---
+++ precache-manifest.ef68cace4a5eb3bcc58c88f79c178e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc86750fad541f9e96f0a8ee60620231",
+    "url": "/sanetechies/habit-tracker/index.html"
+  },
+  {
+    "revision": "3ed9976b2f7edcc07e9b",
+    "url": "/sanetechies/habit-tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9eea291df30c19ca8db3",
+    "url": "/sanetechies/habit-tracker/static/js/2.7ac5b4c0.chunk.js"
+  },
+  {
+    "revision": "96e6acfec058831bf41b",
+    "url": "/sanetechies/habit-tracker/static/js/3.e12f153e.chunk.js"
+  },
+  {
+    "revision": "3ed9976b2f7edcc07e9b",
+    "url": "/sanetechies/habit-tracker/static/js/main.80158efd.chunk.js"
+  },
+  {
+    "revision": "67f106eb6728400dd8b2",
+    "url": "/sanetechies/habit-tracker/static/js/runtime~main.c804904b.js"
+  }
+]);
